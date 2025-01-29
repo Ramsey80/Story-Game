98b4114203b88f7643f21040bda1bc83624f8919
Ramsey80 <Ramseymell554@gmail.com>
2025-01-29 10:40:44 -0800

    Introduction and story statuses

---
+++ gameData.js
@@ -0,0 +1,7 @@
+let playername;
+
+let introtext = "Welcome to the ballista mansion. Today, the 8 of you are invited here to join me for a dinner meal. I will be seeing who shall inherit all of my assets and money. I will be seeing if you have what it takes to be my next successor.";
+let pathText1 = "";
+let pathText2 = "";
+let endText1 = "Congratulatons you are now my successor";
+let endText2 = "I am sorry  but you have failed at becoming my successor";
