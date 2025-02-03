13eeab59395d0582956a611d44444e0058df9482
Ramsey80 <Ramseymell554@gmail.com>
2025-02-03 04:00:18 -0800

    Finished

--- gameData.js
+++ gameData.js
@@ -1,7 +1,11 @@
-let playerName;
+let playerName = "";
 
-let introtext = "Welcome to the ballista mansion. Today, the 8 of you are invited here to join me for a dinner meal. I will be seeing who shall inherit all of my assets and money. I will be seeing if you have what it takes to be my next successor.";
-let pathText1 = "";
-let pathText2 = "";
-let endText1 = "Congratulatons you are now my successor";
-let endText2 = "I am sorry  but you have failed at becoming my successor";
+const introtext = "Welcome to the ballista mansion. Today, the 8 of you are invited here to join me for a dinner meal. I will be seeing who shall inherit all of my assets and money. I will be seeing if you have what it takes to be my next successor.";
+const pathText1 = "You have decided to use the bathroom, you are now dead.";
+const pathText2 = "You make it to the kitchen, and have a conversation with everyone in the room.";
+const pathText3 = "You solve the mystery instantly and know who the killer is.";
+const pathText4 = "You decide to open the refrigerator which activates a booby trap."
+
+const endText1 = "Congratulatons you are now my successor.";
+const endText2 = "I am sorry  but you have failed at becoming my successor and are now dead.";
+const endText3 = "Someone else solved the mystery and you were just a follower.";
--- story.js
+++ story.js
@@ -1,1 +1,36 @@
+
 let playerName = prompt("Please enter your name", "John Doe");
+alert("Hello, " + playerName + "Thank you for joining us today!");
+alert(introText);
+
+let firstChoice = prompt(pathText1 + "\n\nType 'left' for the bathroom or 'right' for the kitchen.").toLowerCase();
+
+if (firstChoice === "left") {
+  // If they chose the bathroom
+  alert("You walk down the left to the bathroom.");
+  alert(endText2);
+
+} else if (firstChoice === "right") {
+  // If they chose the kitchen
+  alert("You take the right path into the kitchen.");
+  
+  // Second decision
+  let secondChoice = prompt(pathText2 + "\n\nType 'open' to open the refrigerator or 'chat' to talk with everyone in the kitchen.").toLowerCase();
+  
+  if (secondChoice === "open") {
+    alert(pathText4);
+    alert(endText2);
+  } else if (secondChoice === "chat") {
+    alert(pathText3);
+    alert(endText1);
+  } else {
+    alert("Invalid input. Let's just say you listened to everyones stories.");
+    alert(pathText3);
+    alert(endText3);
+  }
+
+} else {
+  alert("Invalid choice. Let's assume you went straight ahead.");
+  alert("You take the straight path up to the bedroom all alone");
+  alert(endText3);
+}
